cfbc1ca52ff2483e9047ea091c351071ff15e9bf
JackPoy56 <jackpoyt@gmail.com>
2022-06-09 16:48:56 +0300

    refactored

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const labelEl = document.body.children[0];
-const inputEl = document.body.children[1];
-const buttonEl = document.body.children[2];
+const labelEl = document.querySelector('label');
+const inputEl = document.querySelector('#user__name');
+const buttonEl = document.querySelector('.btn');
+const userTextEl = document.querySelector('.text');
 
 buttonEl.onclick = () => {
-    let userValue = document.body.children[1].value;
+    let userValue = document.querySelector('#user__name').value;
 
     if (userValue === '') {
         userValue = 'Anonymous';
@@ -12,8 +13,9 @@ buttonEl.onclick = () => {
     console.log(userValue);
     
     labelEl.classList.add('delete');
-    labelEl.classList.add('delete');
-    labelEl.classList.add('delete');
+    inputEl.classList.add('delete');
+    buttonEl.classList.add('delete');
+    userTextEl.classList.remove("delete");
 
-    document.body.innerHTML = `<p>Hello, ${userValue}!</p>`;
+    userTextEl.innerText = `Helo, ${userValue}!`;
 }
